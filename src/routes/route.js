0a706c2ef8899d29c953c0f5cf275e595f5148e0
prasadshivendra54 <tshivendra07@gmail.com>
2023-04-20 01:36:16 +0530

    i edit require in a single line that can we import multipls function, and i created one more router thats header is '/test-we' and then call my function, these chenges i did in route.js

--- src/routes/route.js
+++ src/routes/route.js
@@ -1,16 +1,20 @@
 const express = require('express');
 const { route } = require('express/lib/application');
 const router = express.Router();
-const commonFile = require('./common')
+const {name, doSomething} = require('./common')
 
 router.get('/test-me', function (req, res) {
     res.send('This should be working!')
 });
 
 router.get('/test-you', function (req, res) {
-    console.log('This is the constant I created', commonFile.name)
+    console.log('This is the constant I created', name)
     res.send('Hello there, welcome to this application!')
 });
 
+router.get('/test-we', function (req, res) {
+    // console.log('This is the constant I created', doSomething())
+    res.send('Hello there, welcome to this application!' + doSomething())
+});
 
 module.exports = router;
